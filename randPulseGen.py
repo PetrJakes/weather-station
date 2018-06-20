678a954760b7a907d5d523a0f9d1af16ec352324
PetrJakes <petr.jakes.tpc@gmail.com>
2018-06-20 23:16:26 +0200

    ***

--- randPulseGen.py
+++ randPulseGen.py
@@ -6,23 +6,25 @@ import time
 
 pinPwm = 8
 MIN_FREQUENCY = 2 # pulses per second
-MAX_FREQUENCY = 100 # pulses per second
+MAX_FREQUENCY = 70 # pulses per second
 
 GPIO.setmode(GPIO.BCM) # GPIO Numbering Mode Broadcom
 
 GPIO.setup(pinPwm, GPIO.OUT)
-p = GPIO.PWM(pinPwm, 5) # create an object p for PWM on port pinPwm at 50 Hertz  
+p = GPIO.PWM(pinPwm, 50) # create an object p for PWM on port pinPwm at 50 Hertz  
 p.start(50)  # start the PWM on 50 percent duty cycle  
 #p.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work) e.g. 100.5, 5.2  
 #p.ChangeDutyCycle(90)
 
-
+#GPIO.setup(23, GPIO.OUT)
+#q = GPIO.PWM(23, 0.5) # create an object p for PWM on port pinPwm at 1 Hertz  
+#q.start(50)  # start the PWM on 50 percent duty cycle  
 
 try:  
     while True:  
         ran=random.randint(MIN_FREQUENCY, MAX_FREQUENCY)
         p.ChangeFrequency(ran)
-        ran = random.randint(2,10)
+        ran = random.randint(5,20)
         time.sleep(ran)                 # wait randomly        
   
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
--- readAnemoPulses.py
+++ readAnemoPulses.py
@@ -2,92 +2,120 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 import RPi.GPIO as GPIO
-import random
 import time
 
-import numpy as np
 
-
-#WIND_HISTORY_INTERVAL = 10
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(PIN_ANEMO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-#pulsesHistory = np.array([])
-#pulsesHistory = np.append(pulsesHistory , time.time())
-
-
-
-class Anemometer:
-    GPIO.setmode(GPIO.BCM)
+class Anemometer:    
+    GPIO.setmode(GPIO.BCM)     
+    GPIO.setup(23, GPIO.OUT)
+    # create an object q for PWM on port pinPwm at 0.33 Hertz  (wind speed sampling frequency)
+#    q = GPIO.PWM(23, 1.0/3) 
+    q = GPIO.PWM(23, 1) 
+    q.start(50)  # start the PWM on 50 percent duty cycle  
     
-    def __init__(self, pinAnem, windHistoryInterval=60*10, gustHistoryInterval=60*3, pulsesPerRevolution=2, minRpm=0):
-        self.pulsesHistory = []
-        self.pulsesDiffHistory=[]
-        self.gustPulsesHistory=[]
-        self.gustPulsesDiffHistory=[]
-        self.WIND_HISTORY_INTERVAL=windHistoryInterval
-        self.GUST_HISTORY_INTERVAL=gustHistoryInterval
-        self.PULSES_PER_REVOLUTION=float(pulsesPerRevolution)
-        self.minRpm=minRpm
-        self.lastKnownRps=0
-#        self.lastPulseTime = time.time()
-        GPIO.setup(pinAnem, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(pinAnem, GPIO.RISING, callback=self._keepPulseHistory) 
+    def __init__(self,                     
+                    windHistoryInterval=60*10,                     
+                    pulsesPerRevolution=2,
+                    PIN_ANEM=7, 
+                    PIN_RPS_RECORDER=24, 
+                    minRPM=0,                      
+                    maxRPM=3000,
+                    ):
         
+        self.pulsesQueue = []
+        self.rpsQueue=[]
+        self.WIND_HISTORY_INTERVAL=windHistoryInterval        
+        self.PULSES_PER_REVOLUTION=float(pulsesPerRevolution)        
+        self.lastKnownPulseTime=time.time()
+        self.MIN_RPM=minRPM
+        self.MAX_RPM=maxRPM
+        self.MAX_RPS=maxRPM/60
+        self.meanWind= 0
+        self.recentWindGust=0
         
-    def _keepPulseHistory(self, channel):        
+        GPIO.setup(PIN_ANEM, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(PIN_ANEM, GPIO.RISING, callback=self._pulseRecorder) 
+        
+        GPIO.setup(PIN_RPS_RECORDER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(PIN_RPS_RECORDER, GPIO.RISING, callback=self._rpsQueueRecorder) 
+        ##        
+
+    def _pulseRecorder(self, channel):        
+        # pulses from anemometer are recorded as timestamps
         pulseTime=time.time()    
-        self.pulsesHistory.append(pulseTime)
-        self.gustPulsesHistory.append(pulseTime)
-        try:
-            self.pulsesDiffHistory.append(pulseTime-self.lastPulseTime)
-            self.gustPulsesDiffHistory.append(pulseTime-self.lastPulseTime)
-        except AttributeError as e:            
-            pass
+        self.pulsesQueue.append(pulseTime)        
+    
+    def _rpsQueueRecorder(self, channel):
+        """from recorded timestamps a rps que """
+        self._maintainPulsesQueue()
+        if len(self.recentPulsesQueue)>1:            
+            numberOfRotation=len(self.recentPulsesQueue)/self.PULSES_PER_REVOLUTION  
+            dutyTime=(self.recentPulsesQueue[-1]- self.recentPulsesQueue[0])
+#            print "duty time:",  dutyTime
+            averageRps = numberOfRotation/dutyTime
+            if averageRps > self.MAX_RPS: 
+                # to eliminate false short pulses
+                self.rpsQueue.append(0)
+            else:
+                self.rpsQueue.append(averageRps)
+        else:
+            self.rpsQueue.append(0)
+        self._maintainAverageRpsQueue()
+        self._meanWind()
+        self._gustWind()
+        self.instantaneousRps = self.rpsQueue[-1]
+
+    def _maintainAverageRpsQueue(self):
+        queueLenght = len(self.rpsQueue)
+        if queueLenght > self.WIND_HISTORY_INTERVAL:
+            diff = queueLenght - self.WIND_HISTORY_INTERVAL
+            del self.rpsQueue[:diff]
+
+    def _maintainPulsesQueue(self):
+        self.recentPulsesQueue = self.pulsesQueue            
+        self.pulsesQueue=[]
         
-        while self.pulsesHistory[0] < (pulseTime - self.WIND_HISTORY_INTERVAL):
-            self.pulsesHistory.pop(0)
-            self.pulsesDiffHistory.pop(0)        
-        while self.gustPulsesHistory[0] < (pulseTime - self.GUST_HISTORY_INTERVAL):
-            self.gustPulsesHistory.pop(0)
-            self.gustPulsesDiffHistory.pop(0)                
-        self.lastPulseTime = pulseTime        
-            
-    def averageRpm(self):
-        numberOfRotation=len(self.pulsesHistory)/self.PULSES_PER_REVOLUTION        
-        try:            
-            durationOfRotation=(self.pulsesHistory[-1] - self.pulsesHistory[0])
-            rps = numberOfRotation/durationOfRotation
-            if rps == self.lastKnownRps:
-                return 0            
-        except (ZeroDivisionError, IndexError) as e:
-            print e
-            return 0        
-        self.lastKnownRps=rps
-        return rps*60
-
-    def meanWind(self):
-        pass
-
-
+    def _meanWind(self):
+        try:
+            averageRpm=(sum(self.rpsQueue) / len(self.rpsQueue))*60
+        except ZeroDivisionError as e:            
+            self.meanWind = 0
+        self.meanWind = averageRpm
+    
+    def _gustWind(self):
+        gusts=[]
+        for i in range(len(self.rpsQueue)):
+            try:
+                gusts.append(sum(self.rpsQueue[i:i+3])/3)
+            except IndexError as e:
+                pass
+        if gusts:
+            self.recentWindGust=max(gusts)
+        else:
+            self.recentWindGust=0
 
 def main():
     PIN_ANEMO=7
-    WIND_HISTORY_INTERVAL = 60*10 # 10 minutes
-    GUST_HISTORY_INTERVAL= 60*2 # 2 minutes
+    WIND_HISTORY_INTERVAL = 60*10 # 10 minutes    
     PULSES_PER_REVOLUTION = 2
     
-    an=Anemometer(PIN_ANEMO, WIND_HISTORY_INTERVAL, GUST_HISTORY_INTERVAL, PULSES_PER_REVOLUTION)
+    an=Anemometer(WIND_HISTORY_INTERVAL, PULSES_PER_REVOLUTION, PIN_ANEMO)
     try:
-        while True:        
-            try:
-                print an.averageRpm(), "rpm"
-                time.sleep(5)
-            except Exception as e:
-                print e
+       while True:
+            print an.meanWind,  "RPM"
+            print an.recentWindGust
+#            try:
+#                avg = an.averageRpm()
+#                print "average rpm:", avg
+#                
+#            except Exception as e:
+#                print e
+##                print "*****main*****"
+##                print e
+            time.sleep(3)
     except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt      
         GPIO.cleanup()          # when your program exits, tidy up after yourself  
-    print len(an.pulsesHistory)
+    print an.rpsQueue[-1]
 
 
 
