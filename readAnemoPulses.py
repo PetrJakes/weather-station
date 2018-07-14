77b197386583f6be28c60795908a9a74a60a8c9c
PetrJakes <petr.jakes.tpc@gmail.com>
2018-07-14 13:51:29 +0200

    ***

--- readAnemoPulses.py
+++ readAnemoPulses.py
@@ -402,15 +402,6 @@ def birthday():
 
     
 def main():
-#    WIND_HISTORY_INTERVAL = 60*10 # 10 minutes
-#    PULSES_PER_REVOLUTION = 4    
-#    PIN_ANEMO_PULSES_INPUT=22
-#    
-#    PIN_SAMPLING_PULSES_OUTPUT= 18 # Start hardware PWM on a PIN_SAMPLING_PULSES_OUTPUT GPIO (dutycycle 50% set in the code)
-#    SAMPLING_FREQUENCY= 1 # Hz (1 Hz means rps is calculated 1 times per second)
-#    PIN_RPS_SAMPLER_INPUT= 23 
-       
-#    an=Anemometer(WIND_HISTORY_INTERVAL, PULSES_PER_REVOLUTION, PIN_ANEMO_PULSES_INPUT, PIN_SAMPLING_PULSES_OUTPUT, PIN_RPS_SAMPLER_INPUT, SAMPLING_FREQUENCY)
     an=Anemometer()
     vane=WindVane()    
     dispayConnected=True
